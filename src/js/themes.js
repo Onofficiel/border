886eef92f58e88aba87d469afc96d9548f5a8ec3
Kelbaz <81097895+Kodbaz@users.noreply.github.com>
2022-02-11 13:48:56 +0100

    ok

--- src/js/themes.js
+++ src/js/themes.js
@@ -1,7 +1,27 @@
+const container = document.querySelector("#tile-container");
 fetch("./src/data/themes.json")
   .then((r) => {
     return r.json();
   })
   .then((data) => {
-    console.log(data);
+    data.forEach((theme) => {
+      const tile = document.createElement("div");
+      tile.classList.add("tile");
+      tile.innerHTML = `
+      <div class="container">  
+        <div class="text-container">
+          <span class="title">${theme.title}</span>
+          <span class="description">${theme.description}</span>
+        </div>
+        <a class="download" download href="${theme.url}">Download</a>
+      </div>
+
+        <div
+          class="image"
+          style="background: url('${theme.image}');"
+        ></div>
+      `;
+
+      container.appendChild(tile);
+    });
   });
